4a2482ee72cc5af84429747e68b566da92198e1d
BVRIT <BVRIT@PLACEMENTLAB-90>
2020-02-27 16:37:13 +0530

    looping

---
+++ capgemini-looping/src/com/capg/demo/looping/Factorial.java
@@ -0,0 +1,17 @@
+package com.capg.demo.looping;
+
+import java.util.Scanner;
+
+public class Factorial {
+	public static void main(String ar[]) {
+		int sum=0;
+		int i;
+		for(i=1;i<=10;i++)
+		{
+			sum=sum+i;
+		}
+		
+		System.out.println("sum="+sum);
+	}
+
+}
