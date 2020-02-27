21a5e64a156140dd4c2ba5e2abc06be2de704649
BVRIT <BVRIT@PLACEMENTLAB-90>
2020-02-27 16:57:22 +0530

    name

---
+++ name/src/com/name/Name.java
@@ -0,0 +1,14 @@
+package com.name;
+
+import java.util.Scanner;
+
+public class Name {
+
+	public static void main(String[] args) {
+			String me;
+			System.out.println("Enter your name");
+			Scanner k=new Scanner(System.in);
+			me=k.nextLine();
+			System.out.println("Welcome"+me);
+	}
+}
